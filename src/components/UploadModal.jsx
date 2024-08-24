6bf7788cea505928d9f1ce61ece285e41deacf9e
MishaShapiro <mishashapiro@mail.ru>
2024-08-24 13:38:46 +0300

    Add drag and drop

--- src/components/UploadModal.jsx
+++ src/components/UploadModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./UploadModal.module.css"
 import { addFile } from '../redux/FilesReducer';
 import { useDispatch } from 'react-redux';
@@ -6,6 +6,34 @@ import svg from "../images/plus.svg"
 
 const UploadModal = ({onClose}) => {
     const dispatcher = useDispatch()
+
+        const [droppedFile, setDroppedFile] = useState(null);
+        const [isDragging, setIsDragging] = useState(false);
+      
+        const handleDragOver = (event) => {
+          event.preventDefault(); 
+        };
+      
+        const handleDragEnter = () => {
+          setIsDragging(true);
+        };
+      
+        const handleDragLeave = () => {
+          setIsDragging(false);
+        };
+      
+        const handleDrop = (event) => {
+          event.preventDefault();
+          setIsDragging(false);
+      
+          if (event.dataTransfer.files && event.dataTransfer.files.length > 0) {
+            const file = event.dataTransfer.files[0];
+            setDroppedFile(file);
+            dispatcher(addFile(file))
+            onClose()
+            event.dataTransfer.clearData();
+          }
+    };
 
     function handleFileChange (event) {
         const file = event.target.files[0];
@@ -18,7 +46,12 @@ const UploadModal = ({onClose}) => {
             e.stopPropagation()
             onClose()
         }}>
-            <div className={styles.content} onClick={(e) => {e.stopPropagation()}}>
+            <div className={styles.content} onClick={(e) => {e.stopPropagation()}}
+                 onDragOver={handleDragOver}
+                 onDragEnter={handleDragEnter}
+                 onDragLeave={handleDragLeave}
+                 onDrop={handleDrop}
+                 >
                 <h3>Загрузить</h3>
                 <label className={styles.inputfile}> 
                     <input type="file" onChange={handleFileChange} name="file"></input>
